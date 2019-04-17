939f77d7b751e3c23208afc76a7f66ec951fedee
gagandeepsingh09 <gagandeep_singh@zykrr.com>
2019-04-17 22:40:13 +0530

    added confirmed feature

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from uploadNew import *
 from train import *
 from match_faces import *
 from PIL import Image
+import base64
+import json
 import cv2
 class window(QMainWindow):
     def __init__(self):
@@ -57,14 +59,35 @@ class window(QMainWindow):
         t = Thread(target=train)  
         t.start()   
 
+
+    def add_to_confirmed(self,matched):
+        for person in matched:
+            label, img_,loc = person
+            img = cv2.imread(os.path.join('images',img_))
+            image = base64.b64encode(img)
+            root = db.reference('stationID')
+
+            #Create a new entry in confirmed
+            confirmed_req = root.child('ABC123').child('confirmed').child(label[0]).set({
+                'img':json.dumps(image.decode("utf-8"))
+            })
+            create_new_confirmed = db.reference('found').child(label[0]).set({
+                'image':json.dumps(image.decode("utf-8")),
+                'location':loc
+            })
+
+            #Delete from pending
+            # root.child('ABC123').child('pending').delete(label[0])
+
     def match_faces(self):
         matched =  match()
+        self.add_to_confirmed(matched)
         list = QListView(self)
         list.setIconSize(QSize(72,72))
         list.setMinimumSize(400, 380)
         model = QStandardItemModel(list)
         for person in matched:
-            label, img_ = person
+            label, img_, loc = person
             label = label[0].split('@')
             try:
                 name_ = label[0]
@@ -85,7 +108,8 @@ class window(QMainWindow):
                 item1 = QStandardItem("  Name                   : "+name+
                                     "\n  Father's Name    : "+fname+
                                     "\n  Age                      : "+age+
-                                    "\n  Mobile                 : "+mobile )
+                                    "\n  Mobile                 : "+mobile+
+                                    "\n  Location             : "+loc     )
                 
                 icon = QPixmap(os.path.join('images',img_))
 
@@ -94,11 +118,12 @@ class window(QMainWindow):
                 
             except:
                 print("Error in match_faces function")
-            
 
-        
         list.setModel(model)
         list.show()
+
+    
+
 
 App = QApplication(sys.argv)
 w = window()
--- match_faces.py
+++ match_faces.py
@@ -22,20 +22,34 @@ def fetch_faces_fromDB(db_name = 'user'):
     for userID, value in result.items():
         for case, v1 in value.items():
             imgdata = base64.b64decode(v1.get('image'))
+            location = v1.get('location')
             filename = case + str(time.time())[:5]+'.jpg' # I assume you have a way of picking unique filenames
             with open(filename, 'wb') as f:
                 f.write(imgdata)
+            
+            loc = 'locations.txt'
+            to_save = filename+"  "+location
+            with open(loc, 'w') as f:
+                f.write(to_save)
+
 
 def find_key_pts():
     key_pts = []
-    # os.chdir('../')
-    images = os.listdir('images')
-    for image in images:
+    os.chdir('../')
+    with open('images/locations.txt','r') as f:
+        locations = f.read()
+    locations = locations.split('\n')
+
+    for l in locations:
+        l = l.replace('  ',',')
+        l = l.split(',')
+        image = l[0]
+        loc = l[1]
         path = os.path.join('images',image)
         img = face_recognition_api.load_image_file(path)
         faces_encodings = face_recognition_api.face_encodings(img)
         if faces_encodings:
-            key_pts.append([faces_encodings,image])
+            key_pts.append([faces_encodings,image,loc])
     return key_pts
 
 def match():
@@ -47,15 +61,16 @@ def match():
         print("Classifier '{}' does not exist".format(model_name))
         print("Update DB first")
 
-    # fetch_faces_fromDB()
+    print(os.getcwd())
+    fetch_faces_fromDB()
     
     key_pts = find_key_pts()
     matched = []
-    for key,img in key_pts:
+    for key,img,loc in key_pts:
         closest_distances = clf.kneighbors(key)
         is_recognized = [closest_distances[0][0][0] <= 0.5]
         if is_recognized:
             predictions = [(le.inverse_transform(int(pred)).title()) if rec else ("Unknown") for pred, rec in
                     zip(clf.predict(key), is_recognized)]       
-            matched.append([predictions,img])                     
+            matched.append([predictions,img,loc])                     
     return matched    
